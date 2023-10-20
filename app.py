b78f13406026a1bec972e655a0b48ad9a6485957
Yousuf Syed <snuyousuf@gmail.com>
2023-10-20 16:16:32 +0530

    Ready for Prediction

--- app.py
+++ app.py
@@ -20,13 +20,22 @@ def home():
 @app.route('/predict_api',methods=['POST'])
 def predict_api():
     data = request.json['data']
-    print(data)
+    # print(data)
     print(np.array(list(data.values())).reshape(1,-1))
     # new_data = scaler.transform(np.array(list(data.values())).reshape(-1,1))
     new_data = scaler.transform([np.array(list(data.values()))])
     output = regmodel.predict(new_data)
     print(output)
     return jsonify(output[0])
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    data  = [float(x) for x in request.form.values()]
+    # print(data)
+    final_input = scaler.transform([np.array(list(data))])
+    # print(final_input)
+    output = regmodel.predict(final_input)
+    return render_template('home.html', prediction_text = f"The House price is {output[0]}")
 
 if __name__ =="__main__":
     app.run(debug=True)
